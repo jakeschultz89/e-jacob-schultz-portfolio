0461fcf6f81946dcc036a424040fe180b29bf4c2
jakeschultz89 <ejacobschultz@gmail.com>
2021-07-05 15:31:16 -0400

    setting up bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import logo from './logo.svg';
+import profile from './images/profile.jpeg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>E Jacob Schultz portfolio coming soon!</h1>
-    </div>
+    <h2>Portfolio Project</h2>
   );
 }
 
 export default App;
+
+
+// import profile from './images/profile.jpeg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//         <h1>E Jacob Schultz's portfolio coming soon!</h1>
+//         <img src={profile} alt="profile picture"/>
+//     </div>
+//   );
+// }
+
+// export default App;
