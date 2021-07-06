2db02b41e4e7903f70cba507c34655d163613777
jakeschultz89 <ejacobschultz@gmail.com>
2021-07-06 00:44:33 -0400

    adding particles

--- src/App.js
+++ src/App.js
@@ -2,10 +2,28 @@ import profile from './images/profile.jpeg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Particles from 'react-particles-js';
 
 function App() {
   return (
+    <>
+    <Particles
+        params={{
+            particles: {
+                number: {
+                    value: 30,
+                    density: {
+                        enabled: true,
+                        value_area: 900
+                    }
+                }
+            }
+        }}
+    />
     <Navbar />
+    <Header />
+    </>
   );
 }
 
