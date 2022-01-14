33ac1d370c8e94c44f8ae3b0c45821cb647c3380
jakeschultz89 <ejacobschultz@gmail.com>
2022-01-13 23:37:58 -0500

    moved navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,36 +7,27 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-dark fixed-top">
-        <div className="container">
+        <div className="container ">
 
             <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo" /></a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <FontAwesomeIcon icon={ faBars } style={{color: "white"}} />
             </button>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav ml-auto">
+        <div className="collapse navbar-collapse " id="navbarSupportedContent">
+            <ul className="navbar-nav">
                 <li className="nav-item active">
-                    <Link smooth={true} to="home" className="nav-link" href="#">Home <span className="sr-only">(current)</span></Link>
+                    <Link smooth={true} to="home" offset={-100} className="nav-link" href="#">Home <span className="sr-only">(current)</span></Link>
                 </li>
                 <li className="nav-item">
                     <Link smooth={true} to="about" offset={-100} className="nav-link" href="#">About Me</Link>
                 </li>
-                {/* <li className="nav-item">
-                    <Link smooth={true} to="services" offset={-100} className="nav-link" href="#">Services</Link>
-                </li> */}
                 <li className="nav-item">
                     <Link smooth={true} to="experience" offset={-100} className="nav-link" href="#">Experience</Link>
                 </li>
                 <li className="nav-item">
                     <Link smooth={true} to="portfolio" offset={-100} className="nav-link" href="#">Projects</Link>
                 </li>
-                {/* <li className="nav-item">
-                    <Link smooth={true} to="testimonials" offset={-100} className="nav-link" href="#">Testimonials</Link>
-                </li>
-                <li className="nav-item">
-                    <Link smooth={true} to="contacts" offset={-100} className="nav-link" href="#">Contact</Link>
-                </li> */}
             </ul>
         </div>
         </div>
